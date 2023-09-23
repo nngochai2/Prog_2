fad3ad6e499616ebd97236748bfe2027de0bc3dd
dathoangquoc <dathgqc@gmail.com>
2023-09-24 01:34:04 +0700

    Add AdminView

--- View/AdminView.java
+++ View/AdminView.java
@@ -1,141 +1,107 @@
 package View;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class AdminView {
+    public AdminView() {
+    }
 
     public void menu() {
-        // Admin Page
         Scanner scanner = new Scanner(System.in);
+
         String input;
         do {
-            System.out.println("__________________________WELCOME ADMIN" +
-                    "__________________________");
+            System.out.println("__________________________WELCOME ADMIN__________________________");
             System.out.println("ENTER THE NUMBER TO CHOOSE THE OPTION:");
-            System.out.println("""
-                [0] LOG OUT
-                [1] Manage Ports
-                [2] Manage Vehicles
-                [3] Manage Containers
-                [4] Manage Managers
-                """);
+            System.out.println("[0] LOG OUT\n[1] Manage Ports\n[2] Manage Vehicles\n[3] Manage Containers\n[4] Manage Managers\n");
             input = scanner.nextLine().trim();
             switch (input) {
                 case "1":
-                    ports();
-                    break;
+                    this.ports();
                 case "2":
-                    // vehicles()
-                    break;
                 case "3":
-                    // containers()
-                    break;
                 case "4":
-                    // managers()
                     break;
                 default:
                     System.out.println("Invalid input");
             }
-        } while (!input.equals("0"));
+        } while(!input.equals("0"));
+
     }
 
     public void ports() {
-        // Admin Page -> Ports
         Scanner scanner = new Scanner(System.in);
+
         String input;
         do {
-            System.out.println("__________________________ADMIN - PORTS" +
-                    "__________________________");
+            System.out.println("__________________________ADMIN - PORTS__________________________");
             System.out.println("There are currently [number] ports");
             System.out.println("ENTER THE NUMBER TO CHOOSE THE OPTION:");
-            System.out.println("""
-                [0] GO BACK
-                [1] List All Ports
-                [2] Find Port by ID
-                [3] Add Port
-                [4] Remove Port
-                """);
-
+            System.out.println("[0] GO BACK\n[1] List All Ports\n[2] Find Port by ID\n[3] Add Port\n[4] Remove Port\n");
             input = scanner.nextLine().trim();
             switch (input) {
                 case "1":
-                    ports();
+                    this.ports();
                     break;
                 case "2":
-                    portsList();
+                    this.portsList();
                     break;
                 case "3":
-                    addPort();
-                    break;
+                    this.addPort();
                 case "4":
-                    // managers()
                     break;
                 default:
                     System.out.println("Invalid input");
             }
-        } while (!input.equals("0"));
+        } while(!input.equals("0"));
+
     }
 
     public void portsList() {
-        // Admin Page -> Ports -> List All ports
-        System.out.println("__________________________All Ports" +
-                "__________________________");
+        System.out.println("__________________________All Ports__________________________");
         System.out.println("There are currently [number] ports \n");
-        // getPortsList()
-        ports();
-        // Auto go back
+        this.ports();
     }
 
     public void portsByID() {
-        // Admin Page -> Ports -> Find Port by ID
         Scanner scanner = new Scanner(System.in);
-        System.out.println("__________________________Find Port by ID" +
-                "__________________________");
+        System.out.println("__________________________Find Port by ID__________________________");
+
         String input;
         do {
             System.out.println("(Enter ! to cancel)");
             System.out.println("Enter Port ID: ");
             input = scanner.nextLine().trim();
-            // getPortByID(input)
-        } while (!input.equals("!"));
-        // Auto go back
+        } while(!input.equals("!"));
+
     }
 
     public void addPort() {
-        // Admin Page -> Ports -> Add Port
         Scanner scanner = new Scanner(System.in);
-        System.out.println("__________________________Add Port" +
-                "__________________________");
-        String input;
+        System.out.println("__________________________Add Port__________________________");
         scanner.useDelimiter(",");
+
+        String input;
         do {
             System.out.println("(Enter ! to cancel)");
             System.out.println("Seperate values by \" , \" ");
             System.out.println("Enter Port's ID, name, latitude, longitude, storing capacity, landing ability: ");
             input = scanner.nextLine().trim();
-
-            // Pick value from input
             List<String> values = Arrays.asList(input.split(","));
-            String ID = values.get(0);
-            String name = values.get(1);
-            double latitude = Double.valueOf(values.get(2));
-            double longitude = Double.valueOf(values.get(3));
-            double storingCapacity = Double.valueOf(values.get(4));
-            boolean landingAbility = Boolean.valueOf(values.get(5));
+            String ID = (String)values.get(0);
+            String name = (String)values.get(1);
+            double latitude = Double.valueOf((String)values.get(2));
+            double longitude = Double.valueOf((String)values.get(3));
+            double storingCapacity = Double.valueOf((String)values.get(4));
+            boolean var12 = Boolean.valueOf((String)values.get(5));
+        } while(!input.equals("!"));
 
-            // Port.addPort(ID, name, latitude, longitude, storingCapacity, landingAbility)
-
-
-        } while (!input.equals("!"));
     }
 
     public static void main(String[] args) {
         AdminView av = new AdminView();
         av.menu();
-
-        // Admin Page -> Ports -> Remove Port
     }
 }
